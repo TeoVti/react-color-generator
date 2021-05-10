6ab7e470f57e7442b4fcc8993e8450387ddeea75
Teofil-Ioan Vartolomei <teo.vty@gmail.com>
2021-05-10 16:06:07 +0200

    Added color and button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import './App.css';
+import color from 'randomcolor';
+import { useState } from 'react';
 
 function App() {
+  const [oneColor, changeColor] = useState(color.randomColor());
   return (
     <div className="App">
-      <div id="outerContainer">
-        <div id="container">
-          <div className="item">
-            <p>vgrtb</p>
-          </div>
-          <div className="circle"></div>
-          <div className="circle"></div>
-          <div className="circle"></div>
-          <div className="circle"></div>
+      <div id="container">
+        <div className="item">
+          <p>{oneColor}</p>
         </div>
+        <div
+          className="circle"
+          style={{
+            backgroundColor: oneColor.toString(),
+            animationDelay: '-3s',
+          }}
+        />
+        <div
+          className="circle"
+          style={{
+            backgroundColor: oneColor.toString(),
+            animationDelay: '-2s',
+          }}
+        />
+        <div
+          className="circle"
+          style={{
+            backgroundColor: oneColor.toString(),
+            animationDelay: '-1s',
+          }}
+        />
+        <div
+          className="circle"
+          style={{
+            backgroundColor: oneColor.toString(),
+            animationDelay: '0s',
+          }}
+        />
+      </div>
+      <div className="btn">
+        <button
+          className="changeColor"
+          onClick={() => changeColor(color.randomColor())}
+        >
+          Change Color
+        </button>
       </div>
     </div>
   );
 }
-
 export default App;
